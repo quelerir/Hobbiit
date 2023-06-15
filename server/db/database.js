d2eed142e530071b106b3a4cc80c6c3d3fec5492
quelerir <lehandrk@yandex.ru>
2023-06-15 17:32:20 +0300

    deploy

--- server/db/database.js
+++ server/db/database.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: 'postgres',
+    password: 'y6bwYy6D9nIZy2Ui',
+    database: 'postgres',
+    host: 'db.nnddbrfmbevlxglgvlxi.supabase.co',
+    dialect: 'postgres',
   },
 };
--- server/routes/chatRouter.js
+++ server/routes/chatRouter.js
@@ -5,34 +5,42 @@ const { Messages, User } = require('../db/models');
 const chatRouter = express.Router();
 
 chatRouter.get('/:id', async (req, res) => {
-  const { id } = req.params;
-  const userId = req.session.user.id;
-  const messages = await Messages.findAll({
-    where: {
-      [Op.or]: [
-        { subjectchatuser_id: userId, objectchatuser_id: id },
-        { subjectchatuser_id: id, objectchatuser_id: userId },
+  try {
+    const { id } = req.params;
+    const userId = req.session.user.id;
+    const messages = await Messages.findAll({
+      where: {
+        [Op.or]: [
+          { subjectchatuser_id: userId, objectchatuser_id: id },
+          { subjectchatuser_id: id, objectchatuser_id: userId },
+        ],
+      },
+      include: [
+        { model: User, as: 'SubjectChatUser' },
+        { model: User, as: 'ObjectChatUser' },
       ],
-    },
-    include: [
-      { model: User, as: 'SubjectChatUser' },
-      { model: User, as: 'ObjectChatUser' },
-    ],
-    order: [['createdAt', 'ASC']],
-  });
-  res.json(messages);
+      order: [['createdAt', 'ASC']],
+    });
+    res.json(messages);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 chatRouter.post('/addmessage/:id', async (req, res) => {
-  const { id } = req.params;
-  const userId = req.session.user.id;
-  const { message } = req.body;
-  const newMessage = await Messages.create({
-    message,
-    subjectchatuser_id: userId,
-    objectchatuser_id: id,
-  });
-  res.json(newMessage);
+  try {
+    const { id } = req.params;
+    const userId = req.session.user.id;
+    const { message } = req.body;
+    const newMessage = await Messages.create({
+      message,
+      subjectchatuser_id: userId,
+      objectchatuser_id: id,
+    });
+    res.json(newMessage);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = chatRouter;
